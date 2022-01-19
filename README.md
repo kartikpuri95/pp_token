78019ceb81efeeac74a11fe112c246d32431b3bb
mightytux <kartik.puri@incentius.com>
2022-01-19 10:42:39 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Basic Sample Hardhat Project
+
+This project demonstrates a basic erc20 token deployment, you can read more about the project in my article below
+
---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: '<paste_alchemy_url_here>',
+      accounts: ['<paste_your_private_keys_here'],
+    },
+  },
+};
+
+/**
+ * Also note that this file is supposed to be inside gitignore as this file might contain private keys of your wallet
+ * You can also put them inside the environment file to avoid any trouble
+ */
